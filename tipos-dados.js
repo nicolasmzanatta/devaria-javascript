9a09c854e564249ea2e4366faafabbadbd85aad1
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-04-27 21:15:21 -0300

    começo da aula

---
+++ tipos-dados.js
@@ -0,0 +1,20 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false; 
+const nome = "Douglas";
+let copo = null;
+let televisao = undefined;
+let pessoa = {
+    nome: "Rafael"
+}
+
+const symb = Symbol()
+
+console.log("tipo da variavel inteiro", typeof inteiro);
+console.log("tipo da variavel decimal", typeof decimal);
+console.log("tipo da variavel bool", typeof bool);
+console.log("tipo da variavel nome", typeof nome);
+console.log("tipo da variavel copo", typeof copo);
+console.log("tipo da variavel televisao", typeof televisao);
+console.log("tipo da variavel pessoa", typeof pessoa);
+console.log("tipo da variavel symb", typeof symb);
